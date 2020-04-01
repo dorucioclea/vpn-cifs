d613aec86b48f73cb7403b66643ccf9538c289a4
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-04-01 11:03:25 +0300

    chore(vpn): Work on entrypoint to automate the cifs mount

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -6,33 +6,35 @@
 
 cleanup() {
     echo "Container stopped, performing cleanup..."
-    echo "Unmounting "
-    umount /mnt/local_share
+    #
+    # Checks if we have a mount point on /mnt/local_share
+    #
+    [[ "$(df -P /mnt/local_share | tail -1 | cut -d' ' -f 1)" == "//$BUILDTIME_CIFS_HOST/$BUILDTIME_CIFS_PATH" ]] && umount /mnt/local_share || echo "No mount point to clean up"
 
     echo  "Stopping VPN connection:"
-    pkill -SIGINT openconnect
+    killall -SIGINT openconnect
     # Remove default gateway route rule when there is already a PPTP connection
 	# Uncomment line below if your computer is connected to internet through a PPTP connection
 	ip r | grep ppp0 && ip r | grep default | head -n1 | xargs sudo ip r del
+
+    # Send SIGTERM to child/subprocesses
+    kill -- -$$
 }
 
 
 #
-# Trap SIGTERM
+# Trap SIGTERM and SIGINT and execute 
 #
+trap cleanup SIGINT SIGTERM
+echo $BUILDTIME_ANYCONNECT_PASSWORD | openconnect --authgroup $BUILDTIME_ANYCONNECT_GROUP $BUILDTIME_ANYCONNECT_SERVER --user=$BUILDTIME_ANYCONNECT_USER --timestamp --background
 
-trap 'cleanup' SIGTERM
-
-echo $BUILDTIME_ANYCONNECT_PASSWORD | openconnect --authgroup $BUILDTIME_ANYCONNECT_GROUP $BUILDTIME_ANYCONNECT_SERVER --user=$BUILDTIME_ANYCONNECT_USER --timestamp &
-
-sleep 15
+echo "Sleeping 10s. After that will mount the CIFS share"
+sleep 10
 echo "Preparing to mount CIFS share"
-mount -t cifs //$BUILDTIME_CIFS_HOST/$BUILDTIME_CIFS_PATH /mnt/local_share  -o user=$BUILDTIME_ANYCONNECT_USER,password=$BUILDTIME_NYCONNECT_PASSWORD
+mount -t cifs //$BUILDTIME_CIFS_HOST/$BUILDTIME_CIFS_PATH /mnt/local_share  -o user=$BUILDTIME_ANYCONNECT_USER,password=$BUILDTIME_ANYCONNECT_PASSWORD
 echo "Mounted CIFS share"
 
-child_process=$! 
-wait "$child_process"
-
-
-# Cleanup
-cleanup
+#
+# Just keep the process alive
+#
+sleep infinity
