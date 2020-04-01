f2aa3df6d00f44cbf319ea60d07c0f7f139bbe6a
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-04-01 08:39:13 +0300

    chore(entrypoint): Changes done to the entrypoint process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
-echo $ANYCONNECT_PASSWORD | openconnect --background --authgroup $ANYCONNECT_GROUP $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp
-# sleep 10
-# echo 'Connected, attempting to mount'
-# mount -t cifs --verbose //$CIFS_HOST/$CIFS_PATH /mnt/local_share  -o user=$ANYCONNECT_USER,password=$NYCONNECT_PASSWORD
-# echo 'Mounted'
+
+#
+# Define cleanup procedure
+#
+
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    echo "Unmounting "
+    unmount /mnt/local_share
+
+    echo  "Stopping VPN connection:"
+    sudo ps -aef | grep openconnect
+    sudo kill -9 $(pidof openconnect)
+}
+
+
+#
+# Trap SIGTERM
+#
+
+trap 'cleanup' SIGTERM
+
+echo $ANYCONNECT_PASSWORD | openconnect --authgroup $ANYCONNECT_GROUP $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp &
+
+sleep 15
+echo "Preparing to mount CIFS share"
+mount -t cifs //$CIFS_HOST/$CIFS_PATH /mnt/local_share  -o user=$ANYCONNECT_USER,password=$NYCONNECT_PASSWORD
+echo "Mounted CIFS share"
+
+child_process=$! 
+wait "$child_process"
+
+
+# Cleanup
+cleanup
