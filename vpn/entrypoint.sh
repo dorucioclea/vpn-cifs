079f42402829937bd90ea48a4c9aed48492f4164
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-04-01 16:57:13 +0300

    chore(*): Finalise

--- vpn/entrypoint.sh
+++ vpn/entrypoint.sh
@@ -4,37 +4,40 @@
 # Define cleanup procedure
 #
 
-cleanup() {
+do_exit() {
     echo "Container stopped, performing cleanup..."
-    #
-    # Checks if we have a mount point on /mnt/local_share
-    #
+    # #
+    # # Checks if we have a mount point on /mnt/local_share
+    # #
     [[ "$(df -P /mnt/local_share | tail -1 | cut -d' ' -f 1)" == "//$BUILDTIME_CIFS_HOST/$BUILDTIME_CIFS_PATH" ]] && umount /mnt/local_share || echo "No mount point to clean up"
 
     echo  "Stopping VPN connection:"
     killall -SIGINT openconnect
-    # Remove default gateway route rule when there is already a PPTP connection
-	# Uncomment line below if your computer is connected to internet through a PPTP connection
-	ip r | grep ppp0 && ip r | grep default | head -n1 | xargs sudo ip r del
-
-    # Send SIGTERM to child/subprocesses
+    # # Send SIGTERM to child/subprocesses
     kill -- -$$
 }
-
 
 #
 # Trap SIGTERM and SIGINT and execute 
 #
-trap cleanup SIGINT SIGTERM
-echo $BUILDTIME_ANYCONNECT_PASSWORD | openconnect --authgroup $BUILDTIME_ANYCONNECT_GROUP $BUILDTIME_ANYCONNECT_SERVER --user=$BUILDTIME_ANYCONNECT_USER --timestamp --background
+# Run the "exit" command when receiving SIGNAL
+trap "do_exit" SIGINT
+trap "do_exit" SIGKILL
+trap "do_exit" EXIT
+trap "do_exit" SIGTERM
+trap "do_exit" SIGHUP
 
-echo "Sleeping 10s. After that will mount the CIFS share"
+
+echo $BUILDTIME_ANYCONNECT_PASSWORD | openconnect --authgroup $BUILDTIME_ANYCONNECT_GROUP $BUILDTIME_ANYCONNECT_SERVER --user=$BUILDTIME_ANYCONNECT_USER --timestamp --background
+openconnectpid=$!
+
+#
+# During this sleep, the trap won't work as this is a blocking operation
+#
 sleep 10
 echo "Preparing to mount CIFS share"
 mount -t cifs //$BUILDTIME_CIFS_HOST/$BUILDTIME_CIFS_PATH /mnt/local_share  -o user=$BUILDTIME_ANYCONNECT_USER,password=$BUILDTIME_ANYCONNECT_PASSWORD
 echo "Mounted CIFS share"
 
-#
-# Just keep the process alive
-#
-sleep infinity
+# wait forever
+tail -f /dev/null & wait
