af93e4d6d1d315a5fff7f62746ce548c81bdfd5c
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-04-03 15:24:48 +0300

    feat(vpn): Refactor messages and use new variable names

--- vpn/scripts/start-vpn.sh
+++ vpn/scripts/start-vpn.sh
@@ -3,14 +3,14 @@
 # 
 # Set pidfile path
 # 
-PIDFILE="/var/run/openconnect.pid"
+OPENCONNECT_PROCESS_PIDFILE="/var/run/openconnect.pid"
 
-function stop() {
-    echo "Stopping vpn connection"
-    if [ -f ${PIDFILE} ] && ps -p $(< ${PIDFILE}) &> /dev/null; then
+function cleanup_on_exit() {
+    echo "Cleanup vpn connection"
+    if [ -f ${OPENCONNECT_PROCESS_PIDFILE} ] && ps -p $(< ${OPENCONNECT_PROCESS_PIDFILE}) &> /dev/null; then
         # Pid exists, kill process and remove pidfile
         [ ${UID} -ne 0 ] && echo "You must be root to run this script." && exit 1
-        kill $(< ${PIDFILE}) && rm -f ${PIDFILE}
+        kill $(< ${OPENCONNECT_PROCESS_PIDFILE}) && rm -f ${OPENCONNECT_PROCESS_PIDFILE}
     fi
 }
 
@@ -18,41 +18,41 @@ function stop() {
 # Trap SIGTERM and SIGINT and execute 
 #
 # Run the "stop" command when receiving SIGNAL
-trap "stop" SIGINT
-trap "stop" SIGKILL
-trap "stop" EXIT
-trap "stop" SIGTERM
-trap "stop" SIGHUP
+trap "cleanup_on_exit" SIGINT
+trap "cleanup_on_exit" SIGKILL
+trap "cleanup_on_exit" EXIT
+trap "cleanup_on_exit" SIGTERM
+trap "cleanup_on_exit" SIGHUP
 
 #
 # Check we have all environment variables needed
 #
 
-if [[ -z "${BUILDTIME_ANYCONNECT_SERVER}" ]]; then
-  echo 'SERVER is not defined. Process will exit'
+if [[ -z "${BUILDTIME_VPN_SERVER}" ]]; then
+  echo 'Vpn server is not defined. Process will exit'
   exit 1
 fi
 
-if [[ -z "${BUILDTIME_ANYCONNECT_USER}" ]]; then
-  echo 'USER is not defined. Process will exit'
+if [[ -z "${BUILDTIME_VPN_USER}" ]]; then
+  echo 'Vpn user is not defined. Process will exit'
   exit 1
 fi
 
-if [[ -z "${BUILDTIME_ANYCONNECT_PASSWORD}" ]]; then
-  echo 'PASSWORD is not defined. Process will exit'
+if [[ -z "${BUILDTIME_VPN_PASSWORD}" ]]; then
+  echo 'Vpn user password is not defined. Process will exit'
   exit 1
 fi
 
-if [[ -z "${BUILDTIME_ANYCONNECT_GROUP}" ]]; then
-  echo 'GROUP is not defined. Process will exit'
+if [[ -z "${BUILDTIME_VPN_GROUP}" ]]; then
+  echo 'Vpn user group is not defined. Process will exit'
   exit 1
 fi
 
-echo $BUILDTIME_ANYCONNECT_PASSWORD | openconnect --authgroup $BUILDTIME_ANYCONNECT_GROUP $BUILDTIME_ANYCONNECT_SERVER --user=$BUILDTIME_ANYCONNECT_USER --pid-file=${PIDFILE} &
+echo $BUILDTIME_VPN_PASSWORD | openconnect --no-dtls --authgroup $BUILDTIME_VPN_GROUP $BUILDTIME_VPN_SERVER --user=$BUILDTIME_VPN_USER --pid-file=${OPENCONNECT_PROCESS_PIDFILE} &
 VPN_PROCESS=$!
 
-[ $? -ne 0 ] && echo "VPN failed to start!" && \
-    rm -f ${PIDFILE} && exit 1
+[ $? -ne 0 ] && echo "Vpn failed to start!" && \
+    rm -f ${OPENCONNECT_PROCESS_PIDFILE} && exit 1
 
 #
 # Wait non blocking.
